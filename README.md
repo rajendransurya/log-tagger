da73414e496116f912789c8943a01631441699d1
rajendransurya <surya.rajendran@colorado.edu>
2025-02-27 18:57:30 -0700

    modify code to run for bigger files - updated the code to run for bigger files - modified the write function into a single function

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 This repository is for tagging a AWS vpc logs against a lookup table
 The output of the program will be a csv file with the tag counts and port protocol counts in the same directory as the input file.
 
+## Input:
+* The input file is a txt file with the flow logs from AWS VPC. in the same directory. The program expects it to be with the name "flow_logs.txt"
+* The tag list is a csv file with the tag list. The program expects it to be with the name "tag_list.csv"
+
 ## Assumptions:
+* The flow logs are in the format of txt file unlike the log.gz format.
+* The program knows the name of the file and it is hardcoded in the program.
 * For the purpose of this assignment I am having the protocol numbers in a small dictionary. In real world scenario, this dictionary can be replaced with a csv file.
 * If there exists a port protocol combination that isn't in the tag list then it will be tagged as "Unknown"
 * For the port protocol counts I am considering the dstport as the port number. We can also use the srcport for the same purpose.
 
 ## How to run the code:
 * Clone the repository
+* Make sure you have python3 installed in your system
 * Run the following command:
 ```
 python3 log_tagger.py
@@ -16,4 +23,4 @@ python3 log_tagger.py
 
 ## Alternative design:
 * The same program can be moved to a lambda function and can be triggered by a cloudwatch event periodically with the log file as the input.
-* This program can also be run as a cron job in an EC2 instance.
+
--- log_tagger.py
+++ log_tagger.py
@@ -1,3 +1,7 @@
+"""
+This script reads a lookup table and flow logs, tags the logs based on the lookup table, and counts the tags and port/protocol combinations.
+The results are written to separate CSV files.
+"""
 import csv
 from collections import defaultdict
 
@@ -15,9 +19,9 @@ def read_lookup_table(lookup_file):
     return lookup_dict
 
 
-def count_write_flow_logs(flow_log_file, lookup_dict, tagged_logs_file, count_file):
+def count_write_flow_logs(flow_log_file, lookup_dict, tagged_logs_file, port_protocol_count_file):
     """
-    Parse the flow logs and count he tags and port protocol counts
+    Parse the flow logs and count the tags and port protocol counts
     """
     tagged_logs = {}
     port_protocol_counts = defaultdict(int)
@@ -32,30 +36,28 @@ def count_write_flow_logs(flow_log_file, lookup_dict, tagged_logs_file, count_fi
             tag = lookup_dict.get((dstport, protocol), "Unknown")
             tagged_logs[tag]=tagged_logs.get(tag,0)+1
             port_protocol_counts[(dstport, protocol)] += 1
-    write_tag_count(tagged_logs_file, tagged_logs)
-    write_port_protocol_counts(count_file, port_protocol_counts)
-def write_tag_count(tagged_logs_file, tagged_logs):
-    # Write tagged logs file
-    with open(tagged_logs_file, "w",newline="") as file:
+    port_protocol_counts=[[port,protocol,count] for (port,protocol),count in port_protocol_counts.items()]
+    _write_output_file(tagged_logs_file, ["Tag", "Count"], tagged_logs.items())
+    _write_output_file(port_protocol_count_file, ["Port", "Protocol", "Count"], port_protocol_counts)
+    print(f"Tag counts saved to {tagged_logs_file}")
+    print(f"Port/Protocol counts saved to {port_protocol_count_file}")
+
+
+def _write_output_file(filename, headers, data):
+    """
+    Write output file
+    """
+    with open(filename, "w",newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Tag", "Count"])
-        for tag,count in tagged_logs.items():
-            writer.writerow([tag,count])
-    print(f"Tagged logs saved to {tagged_logs_file}")
-def write_port_protocol_counts(count_file, port_protocol_counts):
-    # Write port/protocol count file
-    with open(count_file, "w", newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Port", "Protocol", "Count"])
-        for (port, protocol), count in sorted(port_protocol_counts.items()):
-            writer.writerow([port, protocol, count])
-    print(f"Port/Protocol counts saved to {count_file}")
+        writer.writerow(headers)
+        writer.writerows(data)
 
 
 def parse_logs_write_results(flow_log_file, lookup_file, tagged_logs_file, count_file):
-    """Main function to process logs and generate output files."""
+    """
+    Parse logs and write results
+    """
     lookup_dict = read_lookup_table(lookup_file)
     count_write_flow_logs(flow_log_file, lookup_dict, tagged_logs_file, count_file)
 
-# Example usage:
 parse_logs_write_results("flow_logs.txt", "tag_list.csv", "tag_counts.csv", "port_protocol_counts.csv")
